56e33be6c215e2a66048d289d1fd7d3729e8a82b
mrthnby <meralihanbay@gmail.com>
2024-02-21 01:40:15 +0300

    Initial Commit

---
+++ GuessTheFlagUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessTheFlagUIKit
+//
+//  Created by Mert Ali Hanbay on 21.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
